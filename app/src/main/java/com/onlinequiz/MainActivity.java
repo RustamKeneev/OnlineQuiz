a6d0f782d351a18de53a682ca5b60e14f0762e9d
rustamkeneev <dr.rustamkeneev@gmail.com>
2020-12-04 19:32:06 +0600

    added x-ray fragment xml files

--- app/src/main/java/com/onlinequiz/MainActivity.java
+++ app/src/main/java/com/onlinequiz/MainActivity.java
@@ -1,8 +1,10 @@
 package com.onlinequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button mUrolithiasizButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/onlinequiz/fragments/XRayStoneFragment.java
+++ app/src/main/java/com/onlinequiz/fragments/XRayStoneFragment.java
@@ -2,20 +2,50 @@ package com.onlinequiz.fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.transition.AutoTransition;
+import androidx.transition.TransitionManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.onlinequiz.R;
 
 public class XRayStoneFragment extends Fragment {
 
+    private LinearLayout mExpandableLayoutXRay;
+    private CardView mXRayStoneCard;
+    private ImageView mXRayStoneImage;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_x_ray_stone, container, false);
+        View view = inflater.inflate(R.layout.fragment_x_ray_stone, container, false);
+        mExpandableLayoutXRay = view.findViewById(R.id.by_x_ray_characteristics_layout);
+        mXRayStoneCard = view.findViewById(R.id.x_ray_stone_card);
+        mXRayStoneImage = view.findViewById(R.id.x_ray_stone_image);
+
+
+        mXRayStoneCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mExpandableLayoutXRay.getVisibility()==View.GONE){
+                    TransitionManager.beginDelayedTransition(mXRayStoneCard,new AutoTransition());
+                    mExpandableLayoutXRay.setVisibility(View.VISIBLE);
+                    mXRayStoneImage.setBackgroundResource(R.drawable.ic_arrow_up);
+                }else {
+                    TransitionManager.beginDelayedTransition(mXRayStoneCard,new AutoTransition());
+                    mExpandableLayoutXRay.setVisibility(View.GONE);
+                    mXRayStoneImage.setBackgroundResource(R.drawable.ic_arrow_down);
+                }
+            }
+        });
+        return view;
     }
 }
